cb67f7a704e5d7e2911478d32b3323054eb4cac8
arij <arijchatti63@gmail.com>
2025-03-11 13:44:28 +0100

    Configuration de MongoDB

--- backend/index.js
+++ backend/index.js
@@ -16,3 +16,11 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/study-planner', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
